9914d0ad86565e1d7e74a628a5d402d31ab5196f
abrusu-collins <snillochemingson@gmail.com>
2022-11-14 14:38:43 +0000

    waiting for wifi

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,23 +3,19 @@ function About() {
     return ( 
         <div className="about">
             <div className="about-inner">
-                <div className="about-text">
+            <div className="about-text">
                 <p className="name">Mawuli Gbagbo</p>
                 <p className="description">
                 I am a self-taught pencil artist, illustrator and graphic designer based in Hohoe,Ghana.
                 I'm very passionate about art. My love for the artisan world ranges form creative arts, visual arts, oral arts through to all other forms of art you can think of.
                 My goal is to be able to express various real life situations in different forms of arts so that a lot of audience can relate to what art really is whether you are an artistic person or not.
                 </p>
-
+                <div className="contacts">
+                    
+                </div>
             </div>
             <img src={_5} alt="" />
 
-            </div>
-
-            <div className="contacts">
-                <div className="contacts-inner">
-
-                </div>
             </div>
         </div>
      );
