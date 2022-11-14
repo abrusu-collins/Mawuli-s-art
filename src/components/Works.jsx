6cd575bfff8d4dba2fd8f79285c410ac69c36d61
abrusu-collins <snillochemingson@gmail.com>
2022-11-14 14:03:45 +0000

    renames

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -2,6 +2,9 @@ function Works() {
     return (
         <div className="works">
             <p className="work-title">Recent <br /> Works</p>
+            <div className="work-images">
+                <img src="" alt="" />
+            </div>
         </div>
       );
 }
