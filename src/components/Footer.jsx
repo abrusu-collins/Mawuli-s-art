d8bb9d52bff17c22433ad7f690a38aafa8060ea5
abrusu-collins <snillochemingson@gmail.com>
2022-11-14 17:28:06 +0000

    cool

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
     let year = new Date().getFullYear();
     return ( 
         <footer>
-            <p>&copy; {year} : Collins Abrusu, All rights Reserved</p>
+            <p>&copy; {year} : Collins Abrusu, All rights Reserved.</p>
         </footer>
      );
 }
