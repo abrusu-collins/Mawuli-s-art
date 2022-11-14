7007e36d5b1b4fbefd372dd5065d01dea2278b51
abrusu-collins <snillochemingson@gmail.com>
2022-11-14 13:30:21 +0000

    neat

--- src/components/Dream.jsx
+++ src/components/Dream.jsx
@@ -1,7 +1,16 @@
+import threed from "../images/3d-2.webp"
 function Dream() {
     return (
         <div className="dream">
-            
+            <p className="slogan">
+                Dream it.
+            </p>
+            <p className="design">
+            I Design it.
+            </p>
+            <div className="img">
+            <img src={threed} alt="" />
+            </div>
         </div>
       );
 }
