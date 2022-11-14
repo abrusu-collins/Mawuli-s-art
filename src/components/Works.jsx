501edca8c5ad9c1e126d046cb46d8ef5d3fd8160
abrusu-collins <snillochemingson@gmail.com>
2022-11-14 14:06:14 +0000

    images

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,9 +1,31 @@
+import _1 from "../images/1.jpg";
+import _2 from "../images/2.jpg";
+import _3 from "../images/3.jpg";
+import _4 from "../images/4.jpg";
+import _5 from "../images/5.jpg";
+import _6 from "../images/6.jpg";
+import _7 from "../images/7.jpg";
+import _8 from "../images/8.jpg";
+import _9 from "../images/9.jpg";
+import _10 from "../images/10.jpg";
+
 function Works() {
     return (
         <div className="works">
             <p className="work-title">Recent <br /> Works</p>
             <div className="work-images">
-                <img src="" alt="" />
+                <img src={_1} alt="" />
+                <img src={_2} alt="" />
+                <img src={_3} alt="" />
+                <img src={_4} alt="" />
+                <img src={_5} alt="" />
+                <img src={_6} alt="" />
+                <img src={_7} alt="" />
+                <img src={_8} alt="" />
+                <img src={_9} alt="" />
+                <img src={_10} alt="" />
+
+
             </div>
         </div>
       );
