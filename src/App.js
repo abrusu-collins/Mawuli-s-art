33e274931783550c5f750ed67c63fb8af5ee4263
abrusu-collins <snillochemingson@gmail.com>
2022-11-14 13:57:12 +0000

    works

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./styles/index.css";
 import Hero from "./components/Hero";
 import Dream from "./components/Dream";
+import Works from "./components/Works";
 function App() {
   return (
     <>
     <Hero/>
     <Dream/>
+    <Works/>
     </>
   );
 }
---
+++ src/components/Works.jsx
@@ -0,0 +1,9 @@
+function Works() {
+    return (
+        <div className="works">
+            <p className="work-title">Recent <br /> Works</p>
+        </div>
+      );
+}
+
+export default Works;
