50305ccb86be9ba879ef0bc5c8aa3dbbd23b9945
abrusu-collins <snillochemingson@gmail.com>
2022-11-14 14:26:06 +0000

    nice

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./styles/index.css";
 import Hero from "./components/Hero";
 import Dream from "./components/Dream";
 import Works from "./components/Works";
+import About from "./components/About";
 function App() {
   return (
     <>
     <Hero/>
     <Dream/>
     <Works/>
+    <About/>
     </>
   );
 }
